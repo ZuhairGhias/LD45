5ae1d7780d391c09307d937b3a8d6a9652749008
Zuhair <zuhair.ghias@hotmail.com>
2019-10-05 10:32:09 -0400

    Add basic player scripts

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+
+    public PlayerController pc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pc = GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pc.Move(Input.GetAxis("Horizontal"));
+
+        if(Input.GetKeyDown("e")) pc.Steal();
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float stealRate;
+
+    private Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(float amount)
+    {
+        print("Moving");
+        rb.MovePosition(rb.position + (Vector2.right * amount * moveSpeed * Time.deltaTime));
+    }
+
+    public void Steal()
+    {
+        print("Initiating steal");
+    }
+}
